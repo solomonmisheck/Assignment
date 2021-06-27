cbdb587bc925ce93b4b065bbcbb745fdfd2b6fe2
solomonmisheck <solomisheck@gmail.com>
2021-06-27 23:33:29 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
